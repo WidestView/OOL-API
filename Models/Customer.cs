837034250da936f8dcb99dd0f8f7f4d2728a9c2c
felipebeserra25@gmail.com <felipebeserra25@gmail.com>
2021-08-24 09:29:28 -0300

    Basic Modeling

---
+++ Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OOL_API.Models
+{
+    public class Customer
+    {
+        [Key]
+        public Guid UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
