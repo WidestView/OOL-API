3b5a09767e4004db29b19e707b114cdb27103f4a
figurantpp <renan.r.marcelino2@protonmail.com>
2021-11-02 13:39:48 -0300

    Added swagger annotations to PhotoShootController

--- Controllers/PhotoShootController.cs
+++ Controllers/PhotoShootController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using OOL_API.Data;
 using OOL_API.Models.DataTransfer;
 using OOL_API.Services;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace OOL_API.Controllers
 {
@@ -24,6 +26,8 @@ namespace OOL_API.Controllers
 
 
         [HttpGet]
+        [SwaggerOperation("Lists all available photoshoots")]
+        [SwaggerResponse(200, "The available photoshoots", typeof(IEnumerable<OutputPhotoShoot>))]
         public async Task<IActionResult> ListAll()
         {
             return Ok(
@@ -35,6 +39,9 @@ namespace OOL_API.Controllers
 
         [HttpGet]
         [Route("current")]
+        [SwaggerOperation("Lists all the photoshoots assigned to the current employee")]
+        [SwaggerResponse(200, "The assigned photoshoots", typeof(IEnumerable<OutputPhotoShoot>))]
+        [SwaggerResponse(401, "The employee is not authenticated", typeof(IEnumerable<OutputPhotoShoot>))]
         public async Task<IActionResult> ListCurrentPhotoshoots()
         {
             var employee = await _currentUser.GetCurrentEmployee();
@@ -53,6 +60,9 @@ namespace OOL_API.Controllers
 
 
         [HttpGet("{id}")]
+        [SwaggerOperation("Gets a photoshoot by its ID")]
+        [SwaggerResponse(200, "The photoshoot", typeof(OutputPhotoShoot))]
+        [SwaggerResponse(404, "No photoshoot was found with the given ID")]
         public async Task<IActionResult> GetById(Guid id)
         {
             var result = await _context
@@ -69,6 +79,9 @@ namespace OOL_API.Controllers
         }
 
         [HttpPost("add")]
+        [SwaggerOperation("Inserts a photoshoot")]
+        [SwaggerResponse(404, "Some of the required references were not found")]
+        [SwaggerResponse(200, "The photoshoot entry")]
         public async Task<IActionResult> Add([FromBody] InputPhotoShoot input)
         {
             if (ModelState.IsValid)
