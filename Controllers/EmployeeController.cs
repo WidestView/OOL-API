ccbe8bb48caf2ca115c66ad931b16bd48d5b24a3
figurantpp <renan.r.marcelino2@protonmail.com>
2021-11-06 11:45:38 -0300

    Improved EmployeeController implementation

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OOL_API.Data;
+using OOL_API.Models;
 using OOL_API.Models.DataTransfer;
 using OOL_API.Services;
 using Swashbuckle.AspNetCore.Annotations;
@@ -18,11 +20,17 @@ namespace OOL_API.Controllers
     {
         private readonly StudioContext _context;
         private readonly CurrentUserInfo _currentUserInfo;
+        private readonly IPasswordHash _passwordHash;
 
-        public EmployeeController(CurrentUserInfo currentUserInfo, StudioContext context)
+        public EmployeeController(
+            CurrentUserInfo currentUserInfo,
+            StudioContext context,
+            IPasswordHash passwordHash
+        )
         {
             _currentUserInfo = currentUserInfo;
             _context = context;
+            _passwordHash = passwordHash;
         }
 
         [HttpGet]
@@ -49,12 +57,116 @@ namespace OOL_API.Controllers
         {
             var result = await _context.Employees
                 .Include(employee => employee.User)
+                .Where(row => row.User.Active)
                 .ToListAsync();
 
             return Ok(
                 result
                     .Select(employee => new OutputEmployee(employee))
             );
+        }
+
+        [HttpPut]
+        [Authorize(Roles = AccessLevelInfo.SudoString)]
+        [SwaggerOperation(
+            Summary = "Updates the given employee entry",
+            Description = "This operation requires high level permissions")
+        ]
+        [SwaggerResponse(200, "The updated entry", typeof(OutputEmployee))]
+        [SwaggerResponse(401, "The employee was not authenticated")]
+        [SwaggerResponse(404, "Some of the required references were not found")]
+        public async Task<IActionResult> UpdateGiven([FromBody] InputEmployee input)
+        {
+            var currentEmployee = await _currentUserInfo.GetCurrentEmployee();
+
+            if (currentEmployee == null)
+            {
+                return Unauthorized();
+            }
+
+            if (input.Cpf == currentEmployee.UserId)
+            {
+                ModelState.AddModelError("", "Self updates must be done through the " +
+                                             "/api/employee/self endpoint");
+
+                return BadRequest(ModelState);
+            }
+
+            return await UpdateEntry(input);
+        }
+
+        [HttpPut]
+        [Route("self")]
+        [SwaggerOperation("Updates the entry of the current employee")]
+        [SwaggerResponse(200, "The updated entry", typeof(OutputEmployee))]
+        [SwaggerResponse(401, "The employee was not authenticated")]
+        [SwaggerResponse(404, "Some of the required references were not found")]
+        public async Task<IActionResult> UpdateSelf([FromBody] InputEmployee input)
+        {
+            var currentEmployee = await _currentUserInfo.GetCurrentEmployee();
+
+            if (currentEmployee == null)
+            {
+                return Unauthorized();
+            }
+
+            if (input.Cpf != currentEmployee.UserId)
+            {
+                ModelState.AddModelError("", "Cpf changes are not allowed");
+
+                return BadRequest(ModelState);
+            }
+
+            if (input.AccessLevel != currentEmployee.AccessLevel)
+            {
+                ModelState.AddModelError("", "Self access level changes are not allowed");
+
+                return BadRequest(ModelState);
+            }
+
+            return await UpdateEntry(input);
+        }
+
+
+        private async Task<IActionResult> UpdateEntry(InputEmployee input)
+        {
+            string cpf = input.Cpf;
+
+            var entry = await _context.Employees
+                .FindAsync(cpf);
+
+            await _context.Entry(entry)
+                .Reference(row => row.User)
+                .LoadAsync();
+
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            var occupation = await _context.Occupations.FindAsync(input.OccupationId);
+
+            if (occupation == null)
+            {
+                return NotFound();
+            }
+
+            Employee updated = input.ToModel(occupation, _passwordHash);
+
+            entry.User.Name = updated.User.Name;
+            entry.User.SocialName = updated.User.SocialName;
+            entry.User.BirthDate = updated.User.BirthDate;
+            entry.User.Phone = updated.User.Phone;
+            entry.User.Email = updated.User.Email;
+            entry.User.Password = updated.User.Password;
+            entry.Gender = updated.Gender;
+            entry.Rg = updated.Rg;
+            entry.OccupationId = updated.OccupationId;
+            entry.AccessLevel = updated.AccessLevel;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new OutputEmployee(entry));
         }
     }
 }
